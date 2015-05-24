0138d26548064ecfef3b75d49600d87735b30e4a
josh-martin989 <jmart166@jhu.edu>
2015-05-24 19:27:50 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+##makeCacheMatrix takes a normal R matrix and creates an object that is capable of storing
+##a cached inverse of the given matrix.
+##Example usage: x <- makeCacheMatrix(inputMatrix)
+##The above line will make object x, with get and set functions for the given matrix, as well as
+##for the inverse.  Also, x$getchanged() can be used to obtain a TRUE or FALSE value for whether
+##the matrix has been changed since the inverse was last calculated.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+	inv <- NULL
+	changed <- FALSE
+	set <- function(y){
+		changed <<- TRUE
+		x <<- y
+	}
+	get <- function() x
+	getinverse <- function() inv
+	setinverse <- function(inverse){ 
+		changed <<- FALSE
+		inv <<- inverse
+	}
+	getchanged <- function() changed
+	list(set = set, get = get, getinverse = getinverse, setinverse = setinverse, getchanged = getchanged)
 }
 
+##cacheSolve takes a CacheMatrix created by the makeCacheMatrix function and returns the inverse.
+##If the inverse has already been found for the current matrix, it is returned from cache instead of 
+##recalculating.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+	inv <- x$getinverse()
+	ischanged <- x$getchanged()
+	if(!is.null(inv) && !ischanged){
+		message("getting cached inverse")
+		return(inv)
+	}
+	calc <- x$get()
+	inverse <- solve(calc)
+	x$setinverse(inverse)
+	inverse
 }
